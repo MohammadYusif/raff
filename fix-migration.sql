8b1dde29c2077775fc1da94e58fb5af9eae40052
Mohammad Al-Beladi <Mosefbel@gmail.com>
2026-02-06 17:41:22 +0300

    Delete fix-migration.sql

--- fix-migration.sql
+++
@@ -1,6 +0,0 @@
--- Mark the failed migration as rolled back
--- Run this SQL directly on your production database
-
-UPDATE "_prisma_migrations"
-SET rolled_back_at = NOW()
-WHERE migration_name = '20260109_add_performance_indexes';
